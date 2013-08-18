7717fb05981f29756943319d6e0b8541a3bfb12e
Ivan Baidakou <dmol@gmx.com>
2013-08-18 16:28:11 +0300

    Document Util::Storable

--- lib/App/PerlWatcher/Util/Storable.pm
+++ lib/App/PerlWatcher/Util/Storable.pm
@@ -1,4 +1,5 @@
 package App::PerlWatcher::Util::Storable;
+# ABSTRACT: Used to freeze/thaw PerlWatcher status (watcher memories and shelf of statuses)
 
 use 5.12.0;
 use strict;
@@ -17,10 +18,10 @@ local our %Watchers_Pool;
 
 sub freeze {
     my ($engine) = @_;
-    my %watchers_memories = 
+    my %watchers_memories =
         map { $_ => $_->memory }
         @{ $engine->watchers };
-        
+
     my $stored_items = {
         version           => $App::PerlWatcher::Engine::VERSION // 'dev',
         shelf             => $engine->shelf,
@@ -35,28 +36,28 @@ sub thaw {
     my $watchers = $engine->watchers;
     local our %Watchers_Pool;
     @Watchers_Pool{ @$watchers } = @$watchers;
-    
+
     my $stored_items = eval { Storable::thaw($serialized) };
     return 0 if $@;
-    
+
     my $version = $stored_items->{version} // 'dev';
     return 0
         unless $version eq ($App::PerlWatcher::Engine::VERSION // 'dev');
-    
+
     my %watchers_memories = %{ $stored_items->{watchers_memories} };
     my $shelf = $stored_items->{shelf};
 
     my @actual_watcher_ids
         = grep { $Watchers_Pool{$_} }  keys %watchers_memories;
 
-    $Watchers_Pool{$_}->memory($watchers_memories{$_}) 
+    $Watchers_Pool{$_}->memory($watchers_memories{$_})
         for(@actual_watcher_ids);
-        
+
     my $statuses = $shelf->statuses;
     my $actual_statuses = {};
     @$actual_statuses{ @actual_watcher_ids } = @{$statuses}{ @actual_watcher_ids };
     $shelf->statuses($actual_statuses);
-    
+
     $engine->shelf($shelf);
     return 1;
 }
