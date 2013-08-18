35605afeb19ce8b1e0b56484ce0c935b9af594da
Ivan Baidakou <dmol@gmx.com>
2013-08-18 16:27:52 +0300

    Build results of 8912ec2 (on master)

--- lib/App/PerlWatcher/Util/Storable.pm
+++ lib/App/PerlWatcher/Util/Storable.pm
@@ -2,6 +2,7 @@ package App::PerlWatcher::Util::Storable;
 {
   $App::PerlWatcher::Util::Storable::VERSION = '0.12';
 }
+# ABSTRACT: Used to freeze/thaw PerlWatcher status (watcher memories and shelf of statuses)
 
 use 5.12.0;
 use strict;
@@ -20,10 +21,10 @@ local our %Watchers_Pool;
 
 sub freeze {
     my ($engine) = @_;
-    my %watchers_memories = 
+    my %watchers_memories =
         map { $_ => $_->memory }
         @{ $engine->watchers };
-        
+
     my $stored_items = {
         version           => $App::PerlWatcher::Engine::VERSION // 'dev',
         shelf             => $engine->shelf,
@@ -38,28 +39,28 @@ sub thaw {
     my $watchers = $engine->watchers;
     local our %Watchers_Pool;
     @Watchers_Pool{ @$watchers } = @$watchers;
-    
+
     my $stored_items = eval { Storable::thaw($serialized) };
     return 0 if $@;
-    
+
     my $version = $stored_items->{version} // 'dev';
     return 0
         unless $version eq ($App::PerlWatcher::Engine::VERSION // 'dev');
-    
+
     my %watchers_memories = %{ $stored_items->{watchers_memories} };
     my $shelf = $stored_items->{shelf};
 
     my @actual_watcher_ids
         = grep { $Watchers_Pool{$_} }  keys %watchers_memories;
 
-    $Watchers_Pool{$_}->memory($watchers_memories{$_}) 
+    $Watchers_Pool{$_}->memory($watchers_memories{$_})
         for(@actual_watcher_ids);
-        
+
     my $statuses = $shelf->statuses;
     my $actual_statuses = {};
     @$actual_statuses{ @actual_watcher_ids } = @{$statuses}{ @actual_watcher_ids };
     $shelf->statuses($actual_statuses);
-    
+
     $engine->shelf($shelf);
     return 1;
 }
@@ -72,7 +73,7 @@ __END__
 
 =head1 NAME
 
-App::PerlWatcher::Util::Storable
+App::PerlWatcher::Util::Storable - Used to freeze/thaw PerlWatcher status (watcher memories and shelf of statuses)
 
 =head1 VERSION
 
